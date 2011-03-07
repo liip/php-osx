bec310c560920525c9f21970a405c29824628951
Christian Stocker <me@chregu.tv>
2011-03-07 15:34:42 +0100

    Install File (to be executred by the end user)

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Get packager.tgz";
+curl -s -o /tmp/packager.tgz http://php-osx.liip.ch/packager/packager.tgz
+echo "Unpack packager.tgz";
+echo "Please type in your password, as we want to install this into /usr/local"
+sudo tar -C / -xzf /tmp/packager.tgz
+echo "Start packager (may take some time)";
+sudo /usr/local/packager/packager.py install tools-frontenddev 
+cd $ORIPWD

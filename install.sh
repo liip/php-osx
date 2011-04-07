13856cb6b41a8d3a4ab7d1cc2701c6525448d3a9
Christian Stocker <me@chregu.tv>
2011-04-07 10:59:08 +0200

    check if /usr/local exists

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ echo "Get packager.tgz";
 curl -s -o /tmp/packager.tgz http://php-osx.liip.ch/packager/packager.tgz
 echo "Unpack packager.tgz";
 echo "Please type in your password, as we want to install this into /usr/local"
-sudo tar -C /usr/local -xzf /tmp/packager.tgz
+if [ !  -d /usr/local ] ; then sudo mkdir /usr/local; fi
+sudo  tar -C /usr/local -xzf /tmp/packager.tgz
 echo "Start packager (may take some time)";
 sudo /usr/local/packager/packager.py install tools-frontenddev 
 cd $ORIPWD

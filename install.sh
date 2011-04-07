3ddd30cbfc0879c4fa930240c24ce262d74d4fde
Christian Stocker <me@chregu.tv>
2011-04-07 11:03:00 +0200

    make it possible to upload other TYPE (for beta testing)

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# package type (subfolder in packager)
+
+if [ -z $1 ]; then
+	TYPE=tools
+else
+    TYPE=$1
+fi
+
+
 echo "Get packager.tgz";
 curl -s -o /tmp/packager.tgz http://php-osx.liip.ch/packager/packager.tgz
 echo "Unpack packager.tgz";
@@ -6,5 +16,5 @@ echo "Please type in your password, as we want to install this into /usr/local"
 if [ !  -d /usr/local ] ; then sudo mkdir /usr/local; fi
 sudo  tar -C /usr/local -xzf /tmp/packager.tgz
 echo "Start packager (may take some time)";
-sudo /usr/local/packager/packager.py install tools-frontenddev 
+sudo /usr/local/packager/packager.py install $TYPE-frontenddev 
 cd $ORIPWD

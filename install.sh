d956e62a88bbfe11a9f1afc7c85fade785d16f6f
Christian Stocker <me@chregu.tv>
2012-07-25 15:53:21 +0200

    add 10.8 support

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ fi
 
 if [[ $TYPE != "force" ]]; then
 	OS_VERSION=`sw_vers -productVersion | grep -o 10\..`
-	if [[ $OS_VERSION == "10.7" ]]; then
+	if [[ $OS_VERSION == "10.8" ]]; then
+		echo "Detected OS X Mountain Lion 10.8. Not really tested, but we proceed."
+	elif [[ $OS_VERSION == "10.7" ]]; then
 		echo "Detected OS X Lion 10.7. All ok."
 	elif [[ $OS_VERSION == "10.6" ]]; then
 		echo "Detected OS X Snow Leopard 10.6. All ok."
@@ -51,6 +53,17 @@ if [[ $TYPE = "5.3" ]]; then
 	TYPE=tools
 fi
 
+if [[ $OS_VERSION = "10.8" ]]; then
+	if [[ $TYPE = "5.4" ]]; then
+	    TYPE=5.4-10.8
+	else
+	   echo "***"
+	   echo "On Mountain Lion 10.8, we support currently only PHP 5.4"
+	   echo "please come back later, if you want 5.3"
+	   echo "***"
+	   exit 3
+	fi
+fi
 #if [[ $TYPE = "5.4" ]]; then
 #	TYPE=beta
 #fi
@@ -64,5 +77,5 @@ echo "Please type in your password, as we want to install this into /usr/local"
 if [ !  -d /usr/local ] ; then sudo mkdir /usr/local; fi
 sudo  tar -C /usr/local -xzf /tmp/packager.tgz
 echo "Start packager (may take some time)";
-sudo /usr/local/packager/packager.py install $TYPE-frontenddev 
+sudo /usr/local/packager/packager.py install $TYPE-frontenddev
 cd $ORIPWD

f09c12be32c595867612db7014d7a62119ceedc0
Christian Stocker <me@chregu.tv>
2013-10-26 15:17:29 +0200

    WS only

--- uploadFile.php
+++ uploadFile.php
@@ -18,25 +18,25 @@ upload($src,$dst,$type);
 
 
 function upload($src,$dst,$type = 'text/plain') {
-// Instantiate the class
+    // Instantiate the class
 
-echo "Upload $src to $dst as $type\n";
-$s3 = new AmazonS3();
- 
-$response = $s3->create_object('php-osx.liip.ch', $dst, array(
+    echo "Upload $src to $dst as $type\n";
+    $s3 = new AmazonS3();
+
+    $response = $s3->create_object('php-osx.liip.ch', $dst, array(
     'fileUpload' => $src,
     'acl' => AmazonS3::ACL_PUBLIC,
     'contentType' => $type,
     'storage' => AmazonS3::STORAGE_REDUCED,
-    
-));
- 
-// Success?
 
-if ($response->isOK()) {
-    print "done\n";
-} else {
-    print "failed \n";
-}
+    ));
+
+    // Success?
+
+    if ($response->isOK()) {
+        print "done\n";
+    } else {
+        print "failed \n";
+    }
 
 }

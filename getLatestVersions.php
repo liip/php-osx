1216536f49db06ce09a7f2e98c91d4c60d0607a5
Christian Stocker <me@chregu.tv>
2014-04-05 13:59:04 +0200

    show 5.6 as well in latest releases

--- getLatestVersions.php
+++ getLatestVersions.php
@@ -4,6 +4,8 @@
 require_once 'AWSSDKforPHP/sdk.class.php';
 include("config.inc.php");
 
+print getObject('5.6-10.8');
+print "\n";
 print getObject('5.5-10.8');
 print getObject('5.5');
 print "\n";
@@ -26,15 +28,13 @@ function getObject($src) {
 
 
     if ($response->isOK()) {
-        preg_match("/([5-9]\.[0-9]+)-(10\.[0-9]+){0,1}-*frontenddev-([0-9\.]+)-([0-9]+)/",$response->body,$matches);
-
+        preg_match("/([5-9]\.[0-9]+)-(10\.[0-9]+){0,1}-*frontenddev-([0-9\.(alpha)]+)-([0-9]+)/",$response->body,$matches);
         if ($matches[2] == "") {
             $matches[2] = "10.6/10.7";
         }
         if ($matches[2] == "10.8") {
             $matches[2] = "10.8/10.9";
         }
-
         preg_match("/([0-9]{4})([0-9]{2})([0-9]{2})/",$matches[4],$date);
         $text = "PHP " . $matches[3] . " for OS X " . $matches[2] . " uploaded at " . $date[1] ."-" . $date[2] . "-" . $date[3]  ."\n";
 
